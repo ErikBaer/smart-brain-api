24077e5388516655826775407ad80dafcfdb8989
Erik Baer <erik.baer@yahoo.de>
2020-02-27 09:11:39 +0100

    basic set up bcrypt

--- .history/server_20200227083905.js
+++ .history/server_20200227083905.js
@@ -31,10 +31,6 @@ app.get('/', (re, res) => {
 })
 
 app.post('/signin', (req, res) => {
-    bcrypt.compare("bacon", hash, function(err, res) {
-        //res === true
-    })
-
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
           res.json('success')  
