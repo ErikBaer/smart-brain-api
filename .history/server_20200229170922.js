5c32165dc107714ed6d55fb2c9527936641be829
Erik Baer <erik.baer@yahoo.de>
2020-02-29 17:16:04 +0100

    change database to right host-link

--- .history/server_20200229170922.js
+++ .history/server_20200229170922.js
@@ -10,8 +10,10 @@ const image = require('./controllers/image')
 const db = knex ({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true
+      host : 'postgresql-elliptical-33462',
+      user : 'baerdata',
+      password : 'Tusker1',
+      database : 'smart-brain'
     }
   });
 
