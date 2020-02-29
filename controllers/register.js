a47e259ba538e3b85ad74c4a376f4ae74f6e7c36
Erik Baer <erik.baer@yahoo.de>
2020-02-29 14:33:39 +0100

    Revert "validate against empty input"
    This reverts commit f9fc563ba7e843808dec3ebe535b326cf7b2c99b.

--- controllers/register.js
+++ controllers/register.js
@@ -1,9 +1,5 @@
 const handleRegister = (req,res, db, bcrypt) => {
-    const {email, name, password} = req.body;
-if(!email || !name || !password) {
-    return res.json('bad input')
-}
-
+    const {email, name, password} = req.body
     const hash = bcrypt.hashSync(password);
 
     db.transaction(trx => {
